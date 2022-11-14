23c05d8b208037ec6ccfc01dc3f779a137b62336
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2022-11-14 12:36:47 +0200

    14.11

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('ul');
+
+const orIngredientsEl = ingredients => {
+  return ingredients.map(ingredient => {
+    const titleEl = document.createElement('li');
+    titleEl.textContent = ingredient;
+    titleEl.classList.add('item');
+
+    return titleEl;
+  });
+};
+
+const elements = orIngredientsEl(ingredients);
+ingredientsEl.append(...elements);
+
+console.log(elements);
