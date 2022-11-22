f38129812d194edac6939422f4545c0466870edd
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2022-11-22 15:47:48 +0200

    push

---
+++ src/js/task-07.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+
+inputEl.addEventListener("input", changeTextFontSize);
+
+function changeTextFontSize(event) {
+
+const inputValue = event.currentTarget.value;
+    textEl.style.fontSize = inputValue + "px";
+};
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+const formElements = event.currentTarget.elements;
+const email = formElements.email.value;
+const password = formElements.password.value;
+
+const formData = {
+    email, 
+    password, 
+};
+
+    if (email === "" || password === "") {
+    return alert("Заповніть усі поля");
+};
+
+console.log(formData);
+formEl.reset();
+};
